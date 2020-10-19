a1387055ecd4c156f100b241083a4432b36c69d8
MuramasaSwordMaster <aurelien.depeyrelongue@gmail.com>
2020-10-19 16:00:04 +0200

    changes

--- src/com/lip6/controllers/IndexController.java
+++ src/com/lip6/controllers/IndexController.java
@@ -4,10 +4,7 @@ import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
